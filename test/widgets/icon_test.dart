136917a330c4c2da7bdd4cb8c46aa0b30724e2a2
Victor Augusto <vaas@icomp.ufam.edu.br>
2023-03-08 09:26:34 -0300

    feat: added tests for icon

---
+++ test/widgets/icon_test.dart
@@ -0,0 +1,73 @@
+import "package:client_app_design_system/client_app_design_system.dart";
+import "package:client_app_design_system/utils/enums.dart";
+import "package:flutter/material.dart" as material show Icon;
+import "package:flutter_test/flutter_test.dart";
+import "package:heroicons/heroicons.dart";
+
+import "../utils/test_wrappers.dart";
+
+void main() {
+  group("Icon", () {
+    testWidgets("should render heroicons", (tester) async {
+      final widget = Icon(
+        props: IconProps(
+          variant: IconVariant.heroicons,
+          heroIconsProps: HeroIconsProps(
+            icon: HeroIcons.academicCap,
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+
+      expect(find.byType(HeroIcon), findsOneWidget);
+    });
+
+    testWidgets("should render unicons", (tester) async {
+      final widget = Icon(
+        props: IconProps(
+          variant: IconVariant.unicons,
+          uniconsProps: UniconsProps(
+            icon: UniconsSolid.airplay,
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+
+      expect(find.byType(material.Icon), findsOneWidget);
+    });
+
+    testWidgets("should aseert is throw", (tester) async {
+      // throws assertion error to unicons
+      expect(
+        () {
+          Icon(
+            props: IconProps(
+              variant: IconVariant.unicons,
+              heroIconsProps: HeroIconsProps(
+                icon: HeroIcons.academicCap,
+              ),
+            ),
+          );
+        },
+        throwsAssertionError,
+      );
+
+      // throws assertion error to heroicons
+      expect(
+        () {
+          Icon(
+            props: IconProps(
+              variant: IconVariant.heroicons,
+              uniconsProps: UniconsProps(
+                icon: UniconsSolid.airplay,
+              ),
+            ),
+          );
+        },
+        throwsAssertionError,
+      );
+    });
+  });
+}
