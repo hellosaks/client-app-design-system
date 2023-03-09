de457dc61e2003a7d69450ef8896327fabbe07c3
hoffera <felipe.hoffmeiste@gmail.com>
2023-03-09 19:28:11 -0300

    feat: tests

--- lib/components/indication_filters.dart
+++ lib/components/indication_filters.dart
@@ -1,22 +1,30 @@
 import "package:client_app_design_system/utils/enums.dart";
-import "package:flutter/material.dart";
+import "package:flutter/material.dart" hide Icon;
 import "package:heroicons/heroicons.dart";
 import "package:mix/mix.dart";
 
 import "../theme/theme_saks.dart";
 import "custom_typography.dart";
+import "icon.dart";
 
 class IndicationFilters extends StatefulWidget {
   final String text;
-  final Filter filter;
   final String number;
+  final Filter filter;
+
+  final void Function() onPressed;
 
   const IndicationFilters({
     Key? key,
     required this.text,
     required this.filter,
     required this.number,
+    required this.onPressed,
   }) : super(key: key);
+
+  static const ValueKey circleKey = ValueKey("circleKey");
+  static const ValueKey cardKey = ValueKey("cardKey");
+  static const ValueKey boxKey = ValueKey("boxKey");
 
   @override
   State<IndicationFilters> createState() => _IndicationFiltersState();
@@ -34,80 +42,100 @@ class _IndicationFiltersState extends State<IndicationFilters> {
       mainAxis(MainAxisAlignment.start),
       crossAxis(CrossAxisAlignment.start),
     );
-    return Opacity(
-      opacity: isOpacity ? 0.40 : 1,
-      child: SizedBox(
-        width: 155,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          color: widget.filter == Filter.activated
-              ? ThemeSAKS.colors.special.leaf
-              : ThemeSAKS.colors.primary.sky,
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: VBox(
-              mix: vboxStyles,
-              children: [
-                CustomTypography(
-                  text: widget.text,
-                  variant: TypographyVariant.h4,
-                  weight: FontWeight.bold,
-                  color: ThemeSAKS.colors.primary.sea,
+    final Mix pressableStyles = Mix(rounded(20));
+    final Mix boxStyle =
+        Mix(bgColor(ThemeSAKS.colors.primary.sea), rounded(20));
+
+    return Box(
+      key: IndicationFilters.boxKey,
+      mix: isOpacity ? boxStyle : null,
+      child: Opacity(
+          opacity: isOpacity ? 0.40 : 1,
+          child: SizedBox(
+            width: 155,
+            child: Pressable(
+              mix: pressableStyles,
+              onPressed: () {
+                widget.onPressed();
+                setState(() {
+                  isOpacity = !isOpacity;
+                });
+              },
+              child: Container(
+                key: IndicationFilters.cardKey,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: widget.filter == Filter.activated
+                      ? ThemeSAKS.colors.special.leaf
+                      : ThemeSAKS.colors.primary.sky,
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
-                HBox(
-                  mix: hboxStyles,
+                padding: const EdgeInsets.all(20.0),
+                child: VBox(
+                  mix: vboxStyles,
                   children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: widget.filter == Filter.activated
-                            ? ThemeSAKS.colors.utility.conservative
-                            : ThemeSAKS.colors.primary.sea,
-                      ),
-                      child: Center(
-                        child: CustomTypography(
-                          text: widget.number,
-                          variant: TypographyVariant.h4,
-                          weight: FontWeight.bold,
-                          color: ThemeSAKS.colors.special.leaf,
-                        ),
-                      ),
-                    ),
-                    VBox(
+                    _buildTitle(),
+                    const SizedBox(height: 16),
+                    HBox(
+                      mix: hboxStyles,
                       children: [
-                        const SizedBox(
-                          height: 18,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            setState(() {
-                              isOpacity = !isOpacity;
-                            });
-                          },
-                          splashColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          child: HeroIcon(
-                            HeroIcons.arrowRightCircle,
-                            style: HeroIconStyle.outline,
-                            color: ThemeSAKS.colors.primary.sea,
-                          ),
-                        )
+                        _buildCircle(),
+                        _buildArrow(),
                       ],
                     ),
                   ],
                 ),
-              ],
+              ),
+            ),
+          )),
+    );
+  }
+
+  Widget _buildTitle() {
+    return CustomTypography(
+      text: widget.text,
+      variant: TypographyVariant.h4,
+      weight: FontWeight.bold,
+      color: ThemeSAKS.colors.primary.sea,
+    );
+  }
+
+  Widget _buildCircle() {
+    return Container(
+      key: IndicationFilters.circleKey,
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: widget.filter == Filter.activated
+            ? ThemeSAKS.colors.utility.conservative
+            : ThemeSAKS.colors.primary.sea,
+      ),
+      child: Center(
+        child: CustomTypography(
+          text: widget.number,
+          variant: TypographyVariant.h4,
+          weight: FontWeight.bold,
+          color: ThemeSAKS.colors.special.leaf,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildArrow() {
+    return VBox(
+      children: [
+        const SizedBox(height: 18),
+        Icon(
+          props: IconProps(
+            variant: IconVariant.heroicons,
+            heroIconsProps: HeroIconsProps(
+              icon: HeroIcons.arrowRightCircle,
+              color: ThemeSAKS.colors.primary.sea,
+              size: 20,
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
---
+++ test/widgets/indication_filters_test.dart
@@ -0,0 +1,152 @@
+import "package:client_app_design_system/client_app_design_system.dart";
+import "package:client_app_design_system/components/indication_filters.dart";
+import "package:client_app_design_system/theme/theme_saks.dart";
+import "package:client_app_design_system/utils/enums.dart";
+import "package:flutter/material.dart" hide Icon;
+import "package:flutter_test/flutter_test.dart";
+
+import "../utils/test_wrappers.dart";
+
+void main() {
+  group("IndicationFilters", () {
+    testWidgets(
+      "render component",
+      (WidgetTester tester) async {
+        final widget = IndicationFilters(
+          text: "Pendentes",
+          filter: Filter.pending,
+          number: "2",
+          onPressed: () {},
+        );
+
+        await tester.pumpWidget(wrapWithMaterialApp(widget));
+
+        expect(find.byType(IndicationFilters), findsOneWidget);
+      },
+    );
+
+    testWidgets("onPressed called on tap card", (WidgetTester tester) async {
+      bool pressed = false;
+      final widget = IndicationFilters(
+        text: "Pendentes",
+        filter: Filter.pending,
+        number: "2",
+        onPressed: () {
+          pressed = true;
+        },
+      );
+
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+      await tester.tap(find.byType(IndicationFilters));
+      await tester.pumpAndSettle();
+      expect(pressed, true);
+    });
+
+    testWidgets("expected texts", (WidgetTester tester) async {
+      final widget = IndicationFilters(
+        text: "Pendentes",
+        filter: Filter.pending,
+        number: "2",
+        onPressed: () {},
+      );
+
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+
+      await tester.pumpAndSettle();
+      expect(find.text("Pendentes"), findsOneWidget);
+      expect(find.text("2"), findsOneWidget);
+    });
+
+    testWidgets("expected  attributes", (WidgetTester tester) async {
+      final widget = IndicationFilters(
+        text: "Pendentes",
+        filter: Filter.pending,
+        number: "2",
+        onPressed: () {},
+      );
+
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+      expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is CustomTypography &&
+                widget.text == "Pendentes" &&
+                widget.color == ThemeSAKS.colors.primary.sea &&
+                widget.weight == FontWeight.bold &&
+                widget.variant == TypographyVariant.h4,
+          ),
+          findsOneWidget);
+      expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is CustomTypography &&
+                widget.text == "2" &&
+                widget.color == ThemeSAKS.colors.special.leaf &&
+                widget.weight == FontWeight.bold &&
+                widget.variant == TypographyVariant.h4,
+          ),
+          findsOneWidget);
+
+      expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is Icon &&
+                widget.props.heroIconsProps!.icon ==
+                    HeroIcons.arrowRightCircle &&
+                widget.props.heroIconsProps!.size == 20 &&
+                widget.props.heroIconsProps!.color ==
+                    ThemeSAKS.colors.primary.sea,
+          ),
+          findsOneWidget);
+
+      final container =
+          tester.widget<Container>(find.byKey(IndicationFilters.cardKey));
+      final circle =
+          tester.widget<Container>(find.byKey(IndicationFilters.circleKey));
+
+      expect(
+        container.decoration,
+        BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: ThemeSAKS.colors.primary.sky,
+        ),
+      );
+      expect(
+        circle.decoration,
+        BoxDecoration(
+          shape: BoxShape.circle,
+          color: ThemeSAKS.colors.primary.sea,
+        ),
+      );
+    });
+
+    testWidgets(" Filter.activated attributes", (WidgetTester tester) async {
+      final widget = IndicationFilters(
+        text: "Pendentes",
+        filter: Filter.activated,
+        number: "2",
+        onPressed: () {},
+      );
+      await tester.pumpWidget(wrapWithMaterialApp(widget));
+
+      final container =
+          tester.widget<Container>(find.byKey(IndicationFilters.cardKey));
+      final circle =
+          tester.widget<Container>(find.byKey(IndicationFilters.circleKey));
+      expect(
+        container.decoration,
+        BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: ThemeSAKS.colors.special.leaf,
+        ),
+      );
+      expect(
+        circle.decoration,
+        BoxDecoration(
+          shape: BoxShape.circle,
+          color: ThemeSAKS.colors.utility.conservative,
+        ),
+      );
+    });
+  });
+}
