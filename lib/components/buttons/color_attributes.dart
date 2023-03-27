67f17a41aad8035ba8a4fcb90cb17177da850361
hoffera <felipe.hoffmeiste@gmail.com>
2023-03-27 18:12:44 -0300

    fix

--- lib/components/buttons/color_attributes.dart
+++ lib/components/buttons/color_attributes.dart
@@ -1,5 +1,3 @@
-import "dart:convert";
-
 import "package:flutter/material.dart";
 
 class ColorAttributesButton {
@@ -21,68 +19,4 @@ class ColorAttributesButton {
           !(pressColor == bgColor),
           "background color and press color can't not be equals",
         );
-
-  ColorAttributesButton copyWith({
-    Color? bgColor,
-    Color? pressColor,
-    Color? insideColor,
-    Color? outsideColor,
-  }) {
-    return ColorAttributesButton(
-      bgColor: bgColor ?? this.bgColor,
-      pressColor: pressColor ?? this.pressColor,
-      insideColor: insideColor ?? this.insideColor,
-      outsideColor: outsideColor ?? this.outsideColor,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      "bgColor": bgColor.value,
-      "pressColor": pressColor.value,
-      "insideColor": insideColor.value,
-      "outsideColor": outsideColor?.value,
-    };
-  }
-
-  factory ColorAttributesButton.fromMap(Map<String, dynamic> map) {
-    return ColorAttributesButton(
-      bgColor: Color(map["bgColor"] as int),
-      pressColor: Color(map["pressColor"] as int),
-      insideColor: Color(map["insideColor"] as int),
-      outsideColor: map["outsideColor"] != null
-          ? Color(map["outsideColor"] as int)
-          : null,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory ColorAttributesButton.fromJson(String source) =>
-      ColorAttributesButton.fromMap(
-        json.decode(source) as Map<String, dynamic>,
-      );
-
-  @override
-  String toString() {
-    return "ColorAttributesButton(bgColor: $bgColor, pressColor: $pressColor, insideColor: $insideColor, outsideColor: $outsideColor)";
-  }
-
-  @override
-  bool operator ==(covariant ColorAttributesButton other) {
-    if (identical(this, other)) return true;
-
-    return other.bgColor == bgColor &&
-        other.pressColor == pressColor &&
-        other.insideColor == insideColor &&
-        other.outsideColor == outsideColor;
-  }
-
-  @override
-  int get hashCode {
-    return bgColor.hashCode ^
-        pressColor.hashCode ^
-        insideColor.hashCode ^
-        outsideColor.hashCode;
-  }
 }
