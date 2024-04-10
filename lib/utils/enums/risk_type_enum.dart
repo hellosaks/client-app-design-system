fd4b777b35148c79cf9e4fec2f73d9c85f72891b
Victor Augusto <vaas@icomp.ufam.edu.br>
2024-04-10 16:48:10 -0300

    feat: change abstract class enumdto

--- lib/utils/enums/risk_type_enum.dart
+++ lib/utils/enums/risk_type_enum.dart
@@ -4,20 +4,6 @@ enum RiskType { conservative, moderate, aggressive }
 
 class RiskTypeEnum extends EnumDto<RiskType> {
   @override
-  RiskType parse(String enumType) {
-    switch (enumType) {
-      case "conservative":
-        return RiskType.conservative;
-      case "moderate":
-        return RiskType.moderate;
-      case "aggressive":
-        return RiskType.aggressive;
-      default:
-        return RiskType.conservative;
-    }
-  }
-
-  @override
   String stringfy(RiskType enumType) {
     switch (enumType) {
       case RiskType.conservative:
@@ -28,4 +14,21 @@ class RiskTypeEnum extends EnumDto<RiskType> {
         return "aggressive";
     }
   }
+
+  @override
+  RiskType? canParse(String enumType) {
+    switch (enumType) {
+      case "conservative":
+        return RiskType.conservative;
+      case "moderate":
+        return RiskType.moderate;
+      case "aggressive":
+        return RiskType.aggressive;
+    }
+    return null;
+  }
+
+  @override
+  // TODO: implement defaultEnum
+  RiskType get defaultEnum => RiskType.conservative;
 }
