f916988ef382c2fdbd5a205e12aba8faabe96ec9
Victor Augusto <vaas@icomp.ufam.edu.br>
2024-08-15 15:00:36 -0300

    feat: added colors magnolia

---
+++ lib/theme/magnolia/theme_magnolia.dart
@@ -0,0 +1,11 @@
+// coverage:ignore-file
+
+import "package:client_app_design_system/theme/magnolia/palette.dart";
+import "package:client_app_design_system/theme/shape.dart";
+import "package:client_app_design_system/theme/typography.dart";
+
+class ThemeMagnolia {
+  static final colors = PaletteMagnolia();
+  static final typography = Typography();
+  static final shape = Shape();
+}
