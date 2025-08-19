8d436f46843c0fd344529dfbe6f2dcbdd764417f
Victor Augusto <victoraugustoandradesilva@hotmail.com>
2025-08-19 17:36:48 -0300

    fix: removed json widget

--- lib/components/chart_distribution/info_circular_graph_model.dart
+++ lib/components/chart_distribution/info_circular_graph_model.dart
@@ -1,6 +1,4 @@
-import "package:deep_pick/deep_pick.dart";
 import "package:flutter/material.dart";
-import "package:json_theme/json_theme.dart";
 
 class InfoCircularGraph {
   final Color color;
@@ -13,13 +11,4 @@ class InfoCircularGraph {
           !(value > 100 || value <= 0),
           "value value must be a value between 0 and 100",
         );
-
-  factory InfoCircularGraph.fromJson(Map<String, dynamic> json) {
-    return InfoCircularGraph(
-      color: pick(json, "color").letOrThrow(
-        (pick) => ThemeDecoder.decodeColor(pick.asStringOrThrow())!,
-      ),
-      value: pick(json, "value").asDoubleOrThrow(),
-    );
-  }
 }
